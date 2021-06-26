64abe4a8bad93ffa18cb6d5e8411f556299a3e18
AlekseySharko <alekseysharkodeveloper@gmail.com>
2021-06-26 15:41:48 +0300

    Adds CatalogMainCategory get with subs and prods

--- PortalAsp/Controllers/Catalog/CatalogCategories/CatalogMainCategoryController.cs
+++ PortalAsp/Controllers/Catalog/CatalogCategories/CatalogMainCategoryController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PortalAsp.Controllers.Helpers;
 using PortalAsp.Controllers.Helpers.Catalog;
 using PortalAsp.EfCore.Catalog;
@@ -13,8 +15,22 @@ namespace PortalAsp.Controllers.Catalog.CatalogCategories
         public CatalogMainCategoryController(CatalogContext catalogContext) => CatalogContext = catalogContext;
 
         [HttpGet]
-        public IActionResult GetMainCategories()
+        public IActionResult GetMainCategories([FromQuery] bool includeSubcategories = false, bool includeProductCategories = false)
         {
+            if (includeSubcategories && includeProductCategories)
+            {
+                var result = CatalogContext.CatalogMainCategories.
+                    Include(mc => mc.SubCategories)
+                    .ThenInclude(sc => sc.ProductCategories);
+                BreakSubcategoryInfiniteReferenceCircle(result);
+                return Ok(result);
+            }
+            if (includeSubcategories)
+            {
+                var result = CatalogContext.CatalogMainCategories.Include(mc => mc.SubCategories);
+                BreakSubcategoryInfiniteReferenceCircle(result);
+                return Ok(result);
+            }
             return Ok(CatalogContext.CatalogMainCategories);
         }
 
@@ -29,6 +45,17 @@ namespace PortalAsp.Controllers.Catalog.CatalogCategories
             CatalogContext.CatalogMainCategories.Add(mainCategory);
             CatalogContext.SaveChanges();
             return Ok();
+        }
+
+        private void BreakSubcategoryInfiniteReferenceCircle(IEnumerable<CatalogMainCategory> mainCategories)
+        {
+            foreach (CatalogMainCategory catalogMainCategory in mainCategories)
+            {
+                foreach (var catalogSubCategory in catalogMainCategory.SubCategories)
+                {
+                    catalogSubCategory.ParentMainCategory = null;
+                }
+            }
         }
     }
 }
